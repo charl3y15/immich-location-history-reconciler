45f20c62de7704eb5fb885eecca2df63473af547
Abdelrahman Abounegm <abounegm@yandex-team.ru>
2025-04-10 22:30:35 +0300

    Install PrimeVue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primeuix/themes/aura';
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
-})
+  modules: ["@nuxt/eslint", "@primevue/nuxt-module"],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
+  },
+});
