35c33febef1b258635f45e56b0d6edc152e79839
Abdelrahman Abounegm <abounegm@yandex-team.ru>
2025-04-10 22:42:10 +0300

    Enable v4 compatibility

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 import Aura from '@primeuix/themes/aura';
 
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@primevue/nuxt-module"],
