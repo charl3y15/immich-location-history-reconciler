61cbaf99e7c43e807d46fbc7f1ddf29a206eb161
Abdelrahman Abounegm <abounegm@yandex-team.ru>
2025-04-20 16:10:16 +0300

    Make preview a link to asset on Immich

--- app/components/ImmichAsset.vue
+++ app/components/ImmichAsset.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
-import { AssetMediaSize, viewAsset, type AssetResponseDto } from "@immich/sdk";
+import {
+  AssetMediaSize,
+  viewAsset,
+  defaults as apiOptions,
+  type AssetResponseDto,
+} from "@immich/sdk";
 
 const { asset } = defineProps<{ asset: AssetResponseDto }>();
 const timestamp = computed(() => new Date(asset.fileCreatedAt));
+const href = computed(
+  () => new URL(apiOptions.baseUrl).origin + "/photos/" + asset.id
+);
 
 const {
   data: imgUrl,
@@ -29,18 +37,19 @@ function revokeUrl() {
       <Message v-if="status === 'pending'">Loading...</Message>
       <Message v-else-if="error">{{ error }}</Message>
       <div v-else>
-        <!-- TODO: wrap image in <a> to photo on Immich -->
-        <img
-          class="max-h-80 w-auto m-auto"
-          :src="imgUrl"
-          :height="asset.exifInfo?.exifImageHeight ?? undefined"
-          :width="asset.exifInfo?.exifImageWidth ?? undefined"
-          @load="revokeUrl"
-        />
+        <a :href target="_blank">
+          <img
+            class="max-h-80 w-auto m-auto"
+            :src="imgUrl"
+            :height="asset.exifInfo?.exifImageHeight ?? undefined"
+            :width="asset.exifInfo?.exifImageWidth ?? undefined"
+            @load="revokeUrl"
+          />
+        </a>
       </div>
     </template>
     <template #title>
-      <p style="overflow-wrap: break-word;">{{ asset.originalFileName }}</p>
+      <p style="overflow-wrap: break-word">{{ asset.originalFileName }}</p>
     </template>
     <template #subtitle>
       <p>{{ timestamp.toLocaleString() }}</p>
